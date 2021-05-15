03791dea6c18a906fc7c0dbf77bb591c00f9ace5
BVSMine <bodavulavedasree@gmail.com>
2021-05-15 10:22:14 +0530

    Add project files.

---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dappermvccurd.Models
+{
+    public class EmployeeModel
+    {
+        public int employeeid { get; set; }
+        public string name { get; set; }
+        public string position { get; set; }
+        public int age { get; set; }
+        public int salary { get; set; }
+    }
+}
